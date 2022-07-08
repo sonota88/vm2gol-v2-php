a6b8e6cbe0be50593d07d456ef8d47c36b6fef2d
sonota88 <yosiot8753@gmail.com>
2022-07-09 07:51:40 +0900

    Line number (lexer.php)

--- lexer.php
+++ lexer.php
@@ -18,8 +18,7 @@ function is_kw($str) {
         );
 }
 
-function puts_token($kind, $str) {
-    $lineno = 1; # TODO
+function puts_token($lineno, $kind, $str) {
     $token = [];
     $token[]= $lineno;
     $token[]= $kind;
@@ -32,27 +31,31 @@ function tokenize($src) {
     $pos = 0;
     $rest = "";
     $temp = "";
+    $lineno = 1;
 
     while ($pos < mb_strlen($src)) {
         $rest = mb_substr($src, $pos);
 
-        if (preg_match("/^([ \n]+)/", $rest, $m)) {
+        if (preg_match("/^( +)/", $rest, $m)) {
             $temp = $m[1];
             $pos += mb_strlen($temp);
+        } elseif (preg_match("/^\n/", $rest, $m)) {
+            $lineno += 1;
+            $pos += 1;
         } elseif (preg_match("/^(\/\/.*)/", $rest, $m)) {
             $temp = $m[1];
             $pos += mb_strlen($temp);
         } elseif (preg_match("/^\"(.*)\"/", $rest, $m)) {
             $temp = $m[1];
-            puts_token("str", $temp);
+            puts_token($lineno, "str", $temp);
             $pos += mb_strlen($temp) + 2;
         } elseif (preg_match("/^(-?[0-9]+)/", $rest, $m)) {
             $temp = $m[1];
-            puts_token("int", $temp);
+            puts_token($lineno, "int", $temp);
             $pos += mb_strlen($temp);
         } elseif (preg_match("/^(==|!=|[(){}=;+*,])/", $rest, $m)) {
             $temp = $m[1];
-            puts_token("sym", $temp);
+            puts_token($lineno, "sym", $temp);
             $pos += mb_strlen($temp);
         } elseif (preg_match("/^([a-z_][a-z0-9_]*)/", $rest, $m)) {
             $temp = $m[1];
@@ -61,7 +64,7 @@ function tokenize($src) {
             } else {
                 $kind = "ident";
             }
-            puts_token($kind, $temp);
+            puts_token($lineno, $kind, $temp);
             $pos += mb_strlen($temp);
         } else {
             throw new Exception("Unexpected pattern (${rest})");
